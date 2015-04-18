b284da55a2b3981e94297c4ab381c0b1e0829f6a
Ken Rettberg <kenrettberg@gmail.com>
2015-04-18 10:39:55 -0700

    added seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 
+  ALL_USERS = :address, :lat, :lng, :category
+
+  scope :all_users, -> { select(ALL_USERS) }
+
   def self.send_newsletters
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "user1@example.com", address: "1 First St. 94598",  lat: 37.923583, lng: -122.007946, category: 1)
+User.create(email: "user2@example.com", address: "2 First St. 94598",  lat: 37.922009, lng: -122.007946, category: 2)
+User.create(email: "user3@example.com", address: "3 First St. 94598",  lat: 37.921307, lng: -122.007946, category: 3)
+User.create(email: "user4@example.com", address: "4 First St. 94598",  lat: 37.918793, lng: -122.007946, category: 4)
