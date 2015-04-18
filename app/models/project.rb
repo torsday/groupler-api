e713d1b9060c951fdf9296ff59d5b176cfc0c7ea
Chris Torstenson <c.torsday+github@gmail.com>
2015-04-18 12:51:44 -0700

    bugfix: typo

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  :has_many :bids
-  :has_many :companies, :through => :bids
-  :belongs_to :progression_category
+  has_many :bids
+  has_many :companies, :through => :bids
+  belongs_to :progression_category
 end
