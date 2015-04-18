069ebdc6740e0592f49169c64d801326c1c72255
Ken Rettberg <kenrettberg@gmail.com>
2015-04-18 10:28:51 -0700

    WIP

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def index
+    scope = User.all_users
+
+    # scope = scope.since_quake(Time.zone.at(params[:since].to_i).to_date) if params[:since]
+    # scope = scope.on_day(Time.zone.at(params[:on].to_i).to_date) if params[:on]
+    # scope = scope.mag_over(params[:over]) if params[:over]
+    # if params[:near]
+    #   latlong = params[:near].split(",")
+    #   # binding.pry
+    #   lat = latlong[0].to_f
+    #   # puts lat.class
+    #   lng = latlong[1].to_f
+    #   scope = scope.near(lat, lng)
+    # end
+    #
+    # respond_to do |format|
+    #   format.json { render json: scope }
+    # end
+  end
+end
