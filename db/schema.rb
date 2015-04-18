89267121a45afdda44f65c4d0ac090f5df2e2eae
Ken Rettberg <krettberg@teachtci.com>
2015-04-18 09:43:51 -0700

    added cube and eart distance; set ruby version to 2.1.5

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418161913) do
+ActiveRecord::Schema.define(version: 20150418164207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "cube"
+  enable_extension "earthdistance"
 
   create_table "users", force: :cascade do |t|
     t.string  "email"
