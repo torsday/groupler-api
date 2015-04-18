335113e75dfb0dc2f4627bdbf0eeb754886bb763
Chris Torstenson <c.torsday+github@gmail.com>
2015-04-18 12:54:59 -0700

    add seeds for Company and Project

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,21 @@ User.create(email: "user1@example.com", address: "1 First St. 94598",  lat: 37.9
 User.create(email: "user2@example.com", address: "2 First St. 94598",  lat: 37.922009, lng: -122.007946, category: 2)
 User.create(email: "user3@example.com", address: "3 First St. 94598",  lat: 37.921307, lng: -122.007946, category: 3)
 User.create(email: "user4@example.com", address: "4 First St. 94598",  lat: 37.918793, lng: -122.007946, category: 4)
+
+
+6.times do
+  Project.create(
+    sun_number: 62 + rand(37),
+    mo_elec_bill: 30 + rand(150),
+    zillow_estimate: 200_000 + rand(3_000_000),
+    address: "#{Faker::Address.street_address}, #{Faker::Address.zip_code}",
+    lat: "#{Faker::Address.latitude}",
+    long: "#{Faker::Address.longitude}"
+  )
+end
+
+3.times do
+  Company.create(
+    :name => Faker::Company.name
+  )
+end
