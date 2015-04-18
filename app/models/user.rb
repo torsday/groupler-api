f80c65bf08c9bf0cf41600e08f2e9edd8fcd2720
Ken Rettberg <krettberg@teachtci.com>
2015-04-18 14:33:47 -0700

    Fixed merge-conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ActiveRecord::Base
+  geocoded_by :address, :latitude  => :lat, :longitude => :lng
+
+  validates :email, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+
+  after_validation :geocode
 
   ALL_USERS = :id, :email, :address, :lat, :lng, :category, :price_paid
 
   scope :all_users, -> { select(ALL_USERS) }
+
 
   def self.send_newsletters
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create(email: "user1@example.com", address: "1 First St. 94598",  lat: 37.923583, lng: -122.007946, category: 1)
 User.create(email: "user2@example.com", address: "2 First St. 94598",  lat: 37.922009, lng: -122.007946, category: 2)
-User.create(email: "user3@example.com", address: "3 First St. 94598",  lat: 37.921307, lng: -122.007946, category: 3)
-User.create(email: "user4@example.com", address: "4 First St. 94598",  lat: 37.918793, lng: -122.007946, category: 4)
+User.create(email: "user3@example.com", address: "3 First St. 94598",  lat: 37.921307, lng: -122.007946, category: 3, price_paid: 1200)
+User.create(email: "user4@example.com", address: "4 First St. 94598",  lat: 37.918793, lng: -122.007946, category: 4, price_paid: 1350)
 
 
 23.times do
