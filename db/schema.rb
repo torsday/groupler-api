d291eeec679c9ae9c4d5ad3096c11776dd73443f
Chris Torstenson <c.torsday+github@gmail.com>
2015-04-18 11:45:09 -0700

    create Company db/model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418183632) do
+ActiveRecord::Schema.define(version: 20150418184427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "cube"
   enable_extension "earthdistance"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.integer  "sun_number"
